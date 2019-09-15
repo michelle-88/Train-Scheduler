126ac9d76fbd3f3b6726fed0aedfa2ff12b94598
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-09-15 17:43:45 -0400

    initialized Firebase in my javascript file with the config variable specific to my newly created firebase app.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBx4pfg4wAOxNsas-jHQqMwIL0Ws0vjFA8",
+    authDomain: "trainscheduler-3e64c.firebaseapp.com",
+    databaseURL: "https://trainscheduler-3e64c.firebaseio.com",
+    projectId: "trainscheduler-3e64c",
+    storageBucket: "",
+    messagingSenderId: "947932364860",
+    appId: "1:947932364860:web:149d166ab20a77d0fe8339"
+  };
+
+  firebase.initializeApp(firebaseConfig);
