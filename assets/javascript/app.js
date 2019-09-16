76fd1254940277d5ff4f24a73cc7dfda332a828b
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-09-16 19:04:51 -0400

    declared global variables. created click listener for form submit button. this click listener will run a function that stores the info submitted in the form and pushes this info as a new child to the database. used on child added method in firebase to get snapshot of info in database everytime a new child is added and then add this info to the html table on the index page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 // Initialize Firebase
 var firebaseConfig = {
     apiKey: "AIzaSyBx4pfg4wAOxNsas-jHQqMwIL0Ws0vjFA8",
@@ -15,17 +17,73 @@ var firebaseConfig = {
 var database = firebase.database();
 
 // Initial variables to store info for each train
-var name = "";
-var place = "";
-var frequency = 0;
-var nextArrival = "";
-var minutesAway = 0;
+var name;
+var place;
+var frequency;
+var firstTime;
+var nextArrival;
+var minutesAway;
 
 // Click listener for train form submit button. This will capture the values inputted into the form and update database accordingly (addChild).
+$("#add-train").on("click", function(event){
+  event.preventDefault();
 
+  name = $("#name-input").val().trim();
+  place = $("#place-input").val().trim();
+  frequency = $("#frequency-input").val().trim();
+  firstTime = $("#time-input").val().trim();
 
-// Firebase .on("value") method that will display database info on index page on page load and update index page when any database values change.
+  console.log(name);
+  console.log(place);
+  console.log(frequency);
+  console.log(firstTime);
+
+  database.ref().push({
+    trainName: name,
+    destination: place,
+    dbfrequency: frequency,
+    dbfirstTime: firstTime
+  });
+
+  $("#name-input").val("");
+  $("#place-input").val("");
+  $("#frequency-input").val("");
+  $("#time-input").val("");
+
+});
+
+// Firebase .on("child_added") method that will display database info on index page on page load and update index page when any database values change.
+database.ref().on("child_added", function(snapshot) {
+  console.log(snapshot.val());
+
+  // store everything into a vriable
+  var tName = snapshot.val().trainName;
+  var tPlace = snapshot.val().destination;
+  var tFrequency = snapshot.val().dbfrequency;
+  var tFirstTime = snapshot.val().dbfirstTime;
+  
+  // ******Next arrival and Minutes Away Calculations here******
+
+  // display results inside the table
+  var tr = $("<tr>");
+
+  // create vars to hold table elements and content
+  tr.append(
+    "<td>" + tName + "</td>",
+    "<td>" + tPlace + "</td>",
+    "<td>" + tFrequency + "</td>",
+    "<td> To Be Calculated </td>",
+    "<td> To Be Calculated </td>"
+  )
+
+  $("tbody").append(tr);
+  // append all table data to table row
+  // append to tbody element
+  
+});
 
 // Function using moment.js, current time (on page load?), and frequency to calculate next train arrival time
 
 // Function using moment.js, next arrival time, and current time (on page load?) to calculate minutes away
+
+});
